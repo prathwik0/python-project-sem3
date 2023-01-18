258e1154165fc891aa89604e7fe019e7e4cfbeac
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-19 00:15:33 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='myproject',
+    version='0.1',
+    install_requires=[
+        'pyttsx3',
+        'datetime',
+        'speech_recognition',
+        'wikipedia',
+        'webbrowser',
+        'os',
+        'random',
+        'pyautogui'
+    ],
+)
