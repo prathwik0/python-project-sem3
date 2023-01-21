bdb55ab46979fba57dfea42e645c3a3a1062a7ab
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-21 12:24:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -79,23 +79,27 @@ def screenshot():
     img.save('./ss.png')
 
 
+# def takecommand():
+#     r = sr.Recognizer()
+#     with sr.Microphone() as source:
+#         print("Listening...")
+#         r.pause_threshold = 1
+#         audio = r.listen(source)
+
+#     try:
+#         print("Recognizing...")
+#         query = r.recognize_google(audio, language="en-in")
+#         print(query)
+#         return query
+
+#     except Exception as e:
+#         print(e)
+#         speak("Please say that again")
+#         return "Try Again"
+
 def takecommand():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 1
-        audio = r.listen(source)
-
-    try:
-        print("Recognizing...")
-        query = r.recognize_google(audio, language="en-in")
-        print(query)
-        return query
-
-    except Exception as e:
-        print(e)
-        speak("Please say that again")
-        return "Try Again"
+    n = input("enter string: ")
+    return n
 
 
 def main():
